bbd9ea7e6db07b73ce0b10e3af197165116534bc
eid-developer <eid-developer@idnow.io>
2025-07-29 10:34:20 +0200

    Release - Update SDK to version 1.0.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.1/IDnowEID.xcframework.zip",
-            checksum: "1f9406dd7f5b9324b293cb650d2b894cb6bc6a481587ac67e8e3782d8b27fe54"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.2/IDnowEID.xcframework.zip",
+            checksum: "7e64b1a2093d7d9358773c6f879ce9c97a3dcad6c5b52b8a81d366c82edacd80"
         )
     ]
 )
