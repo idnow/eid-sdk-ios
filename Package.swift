5e0ad093b1d71dde804db140b11c44e6b5b0a56f
eid-developer <eid-developer@idnow.io>
2026-01-08 15:36:47 +0100

    Release - Update SDK to version 1.2.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -10,13 +10,35 @@ let package = Package(
         .library(
             name: "IDnowEID",
             targets: ["IDnowEID"]
+        ),
+        .library(
+            name: "IDnowEIDDynamic",
+            targets: ["IDnowEIDDynamicWrapper"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", exact: "3.6.1"),
+        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "2.1.3")
     ],
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.1.1/IDnowEID.xcframework.zip",
-            checksum: "f7777d6678c3c6b0329d2af024107df560690109376b0c1a49ddc785cfa0ed81"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.2.0/IDnowEID.xcframework.zip",
+            checksum: "9fb6fe35e6c1cef5fdc2d68487c3ffd1e1c4ecc9dc55004d1dec3d79a9620075"
+        ),
+        .binaryTarget(
+            name: "IDnowEIDDynamic",
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.2.0/IDnowEIDDynamic.xcframework.zip",
+            checksum: "3f42c8ed385ce66ecd2fedd40feb4d742baeba57e2135fee33fc69cf789ef334"
+        ),
+        .target(
+            name: "IDnowEIDDynamicWrapper",
+            dependencies: [
+                "IDnowEIDDynamic",
+                .product(name: "OpenSSL", package: "OpenSSL"),
+                .product(name: "SunflowerUIKit", package: "sunflower-sdk-ios")
+            ],
+            path: "sources-dynamic"
         )
     ]
 )
